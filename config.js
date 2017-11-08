1a7f907cbab4b618b315b68713d233c80224ea2a
Govind Kanshi <govindk@microsoft.com>
2017-11-08 11:49:36 -0800

    Update config with fillme in

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.username = 'gcas';
-config.password = 'KP12cSbS3q5m8agIzcglAVSJJNL8T13TCZb2Q1fuqp3ShR2my0cqA4wVN3jFwhRdOjETx8VM5oxVgAzzZJxK5w==';
-config.contactPoint = 'cosmosdbcassandrademo.cloudapp.net:10350';
+config.username = '<FillMEIN>';
+config.password = '<FillMEIN>';
+config.contactPoint = '<FillMEIN>:10350';
 module.exports = config;
